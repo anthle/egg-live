dc0c1082b3ff38109360796acafa865b47071c31
anthle <su810152974@gmail.com>
2023-07-20 22:51:48 +0800

    chore: database init

--- config/config.default.js
+++ config/config.default.js
@@ -10,7 +10,7 @@ module.exports = (appInfo) => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {}
+  const config = module.exports = {}
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = `${appInfo.name}_1689859901495_4488`
@@ -35,6 +35,30 @@ module.exports = (appInfo) => {
   config.cors = {
     origin: '*',
     allowMethods: 'GET, PUT, POST, DELETE, PATCH',
+  }
+
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    username: 'root',
+    password: 'root',
+    port: 3306,
+    database: 'egg-live',
+    // 中国时区
+    timezone: '+08:00',
+    define: {
+      // 取消数据表名复数
+      freezeTableName: true,
+      // 自动写入时间戳 created_at updated_at
+      timestamps: true,
+      // 字段生成软删除时间戳 deleted_at
+      // paranoid: true,
+      createdAt: 'created_time',
+      updatedAt: 'updated_time',
+      // deletedAt: 'deleted_time',
+      // 所有驼峰命名格式化
+      underscored: true,
+    },
   }
 
   return {
--- config/plugin.js
+++ config/plugin.js
@@ -10,4 +10,9 @@ module.exports = {
     enable: true,
     package: 'egg-cors',
   },
+
+  sequelize: {
+    enable: true,
+    package: 'egg-sequelize',
+  },
 }
